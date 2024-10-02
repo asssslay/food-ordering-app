369dca20376489f33cfd8997c7ca1c5b86aba4cd
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-10-02 21:30:12 +0300

    Complete navigation

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,10 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Header } from "../components/header";
+import { Header } from "../components/Header";
+import Home from "../pages/Home";
+import Menu from "../pages/Menu";
+import Cart from "../pages/Cart";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PaymentSuccess from "../pages/PaymentSuccess";
 
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+      </Routes>
     </BrowserRouter>
   );
 };

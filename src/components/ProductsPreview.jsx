138f56209e7d2c41abec66660ef1442dad9c9f95
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-10-18 15:08:53 +0300

    Successful product preview

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -6,12 +6,16 @@ export const ProductsPreview = () => {
   useEffect(() => {
     fetch("http://localhost:8080/api/products")
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => setProducts(data?.data))
       .catch((e) => console.log(e));
   }, []);
   return (
     <div className="container mx-auto pb-4 w-2/3 text-white">
       <h2>Products</h2>
+      {products.length > 0 &&
+        products.map((product, index) => {
+          return <div key={index}>{product.name}</div>;
+        })}
     </div>
   );
 };

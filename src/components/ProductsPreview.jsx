81a1a81e1fe0ab5e0f945e1938008a79777aad55
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-10-18 15:42:27 +0300

    Add products slider

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -1,7 +1,31 @@
 import React, { useState, useEffect } from "react";
+import { ProductsPreviewCard } from "./ProductPreviewCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export const ProductsPreview = () => {
   const [products, setProducts] = useState([]);
+
+  // Carousel response
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/api/products")
@@ -10,12 +34,17 @@ export const ProductsPreview = () => {
       .catch((e) => console.log(e));
   }, []);
   return (
-    <div className="container mx-auto pb-4 w-2/3 text-white">
-      <h2>Products</h2>
-      {products.length > 0 &&
-        products.map((product, index) => {
-          return <div key={index}>{product.name}</div>;
-        })}
+    <div className="container mx-auto pb-4 w-2/3 text-white bg-neutral-950">
+      <Carousel responsive={responsive}>
+        {products.length > 0 &&
+          products.map((product, index) => {
+            return (
+              <div className="w-full p-3">
+                <ProductsPreviewCard key={index} product={product} />
+              </div>
+            );
+          })}
+      </Carousel>
     </div>
   );
 };
